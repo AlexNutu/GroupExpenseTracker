1c94d28844ee8e0f4045f3ca142d0fc407c97a40
Alex Nutu <alexg.nutu@gmail.com>
2020-01-23 00:36:04 +0200

    Small fix to show title in app bar layout

--- app/src/main/java/com/example/expensetracker/SettingsActivity.java
+++ app/src/main/java/com/example/expensetracker/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.expensetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,8 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
